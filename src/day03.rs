8f0a4195df0976bf23c9702e710451424fe36827
Joel José Ginga Castanheira <joel.castanheira@thenavigatorcompany.com>
2024-12-05 14:52:10 +0000

    day 03 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -1,4 +1,5 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use regex::Regex;
 
 #[aoc_generator(day3)]
 pub fn generate_input(input: &str) -> Vec<String> {
@@ -6,15 +7,45 @@ pub fn generate_input(input: &str) -> Vec<String> {
 }
 
 #[aoc(day3, part1)]
-pub fn solve_part1(input: &[String]) -> usize {
-    input.len()
+pub fn solve_part1(input: &[String]) -> i32 {
+    //regex patern 
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    input.into_iter()
+         .flat_map(|line| {
+            re.captures_iter(line).filter_map(|cap| {
+                let num1 = cap[1].parse::<i32>().ok()?;
+                let num2 = cap[2].parse::<i32>().ok()?;
+                Some(num1*num2)
+            })
+         })
+         .sum()
 }
 
 
-/*
+
 //part 2
 #[aoc(day3, part2)]
-pub fn solve_part2(input: &[String]) -> usize {
-    0 as usize
+pub fn solve_part2(input: &[String]) -> i32 {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
+    let mut skip = false;
+    
+    re.captures_iter(&input.join(""))
+      .filter_map(|cap| {
+          let token = &cap[0];
+          if token == "don't()" {
+              skip = true; 
+              None
+          } else if token == "do()" {
+              skip = false;
+              None
+          } else if !skip {
+              let num1: i32 = cap[1].parse().unwrap(); 
+              let num2: i32 = cap[2].parse().unwrap(); 
+              Some(num1 * num2) 
+          } else {
+              None 
+          }
+      })
+      .sum()
 }
-*/
